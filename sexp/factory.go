0bda3288ea71f48ab1338188c911f089ae5fe763
Jia Lu <lujiacn@gmail>
2016-01-27 10:18:36 +0000

    update sexp from roger project

--- sexp/factory.go
+++ sexp/factory.go
@@ -4,6 +4,7 @@ package sexp
 import (
 	"encoding/binary"
 	"errors"
+	//	"log"
 	"strconv"
 )
 
@@ -14,16 +15,28 @@ func Parse(buf []byte, offset int) (interface{}, error) {
 	return obj, err
 }
 
-func getLength(buf []byte, offset int, isLong bool) int {
-	if isLong {
-		return int(binary.LittleEndian.Uint32(buf[offset+1 : offset+4]))
+func getLength(buf []byte, offset int, isLong bool) (int, error) {
+	throwError := func() (int, error) {
+		return offset, errors.New("Abruptly reached end of buffer")
 	}
-	return int(uint32(buf[offset+1]) | (uint32(buf[offset+2]) << 8) | (uint32(buf[offset+3]) << 16))
+	if isLong {
+		if len(buf) <= offset+4 {
+			return throwError()
+		}
+		return int(binary.LittleEndian.Uint32(buf[offset+1 : offset+5])), nil
+	}
+	if len(buf) <= offset+3 {
+		return throwError()
+	}
+	return int(uint32(buf[offset+1]) | (uint32(buf[offset+2]) << 8) | (uint32(buf[offset+3]) << 16)), nil
 }
 
 func parseReturningOffset(buf []byte, offset int) (interface{}, int, error) {
 	isLong := ((buf[offset] & 64) != 0)
-	length := getLength(buf, offset, isLong)
+	length, err := getLength(buf, offset, isLong)
+	if err != nil {
+		return nil, len(buf), err
+	}
 	xt := expressionType(buf[offset] & 63)
 
 	hasAtt := ((buf[offset] & 128) != 0)
@@ -33,6 +46,8 @@ func parseReturningOffset(buf []byte, offset int) (interface{}, int, error) {
 		offset = offset + 4
 	}
 	end := offset + length
+
+	//	log.Printf("parseREXP: type=%v, len=%v, hasAtt=%v, isLong=%v\n", xt, length, hasAtt, isLong)
 
 	var attr interface{}
 	if hasAtt {
@@ -48,6 +63,21 @@ func parseReturningOffset(buf []byte, offset int) (interface{}, int, error) {
 	}
 	if xt == xtInt {
 		return parseInt(buf, offset, end)
+	}
+	if xt == xtString {
+		return parseString(buf, offset, end)
+	}
+	if xt == xtLang {
+		return parseLang(buf, offset, end)
+	}
+	/*
+		if xt == xtS4 {
+			//TODO
+			return nil, offset, errors.New("Unimplemented expression type: XT_S4")
+		}
+	*/
+	if xt == xtClos {
+		return nil, offset, errors.New("Unimplemented expression type: XT_CLOS")
 	}
 	if xt == xtSymName {
 		return parseSymName(buf, offset, end)
@@ -67,8 +97,33 @@ func parseReturningOffset(buf []byte, offset int) (interface{}, int, error) {
 	if xt == xtVector {
 		return parseVector(attr, buf, offset, end)
 	}
+	if xt == xtListNoTag {
+		return parseListNoTag(attr, buf, offset, end)
+	}
 	if xt == xtListTag {
 		return parseListTag(buf, offset, end)
 	}
+
+	if xt == xtLangNoTag {
+		return parseListNoTag(attr, buf, offset, end)
+	}
+
+	if xt == xtLangTag {
+		return parseLangTag(buf, offset, end)
+	}
+	if xt == xtExpVector {
+		return parseExpVector(attr, buf, offset, end)
+	}
+	if xt == xtRaw {
+		return parseRaw(buf, offset, end)
+	}
+	if xt == xtComplexArray {
+		return parseComplexArray(buf, offset, end)
+	}
+
+	if xt == xtUnknown {
+		return parseUnknown(buf, offset, end)
+	}
+
 	return nil, offset, errors.New("Unsupported expression type: " + strconv.Itoa(int(xt)))
 }
